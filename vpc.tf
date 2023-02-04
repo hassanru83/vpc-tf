837dd4d279fc3ed3212ae930cbbb50b10b904dcf
Hassan <hassan.ru83@gmail.com>
2023-02-04 10:11:09 -0500

    count

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,14 @@
+
 resource "aws_vpc" "my-teraformvpc" {
   cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "main" {
+  count = 4
+  vpc_id     = aws_vpc.my-teraformvpc.id
+  cidr_block = "10.0.${count.index}.0/24"
+
+  tags = {
+    Name = "subnet-${count.index}"
+  }
 }
