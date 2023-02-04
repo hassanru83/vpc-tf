b43d1806c5e2c464c5e5f162911161861b2f89c8
Hassan <hassan.ru83@gmail.com>
2023-02-04 11:33:40 -0500

    vpc-subnet-ec2

--- vpc.tf
+++ vpc.tf
@@ -4,11 +4,24 @@ resource "aws_vpc" "my-teraformvpc" {
 }
 
 resource "aws_subnet" "main" {
-  count = 4
+  count      = 4
   vpc_id     = aws_vpc.my-teraformvpc.id
   cidr_block = "10.0.${count.index}.0/24"
 
   tags = {
-    Name = "subnet-${count.index}"
+    Name = "subnet-${count.index + 1}"
   }
+}
+
+data "aws_subnet" "name" {
+  filter {
+    name = "tag:Name"
+    values = ["subnet-2"]
+  }
+}
+
+resource "aws_instance" "foo" {
+  ami           = "ami-0b0dcb5067f052a63" # us-east-1
+  instance_type = "t2.micro"
+  subnet_id     = data.aws_subnet.name.id
 }
